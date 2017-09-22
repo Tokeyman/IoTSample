8195f1bd9488da44b92d64ee85d0417d79f2329e
Tokeyman <guan_shiyang@msn.com>
2017-09-22 09:17:31 +0800

    WcfSample

---
+++ WcfSample/WindowsServiceHost/Install.bat
@@ -0,0 +1,3 @@
+﻿%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe WindowsServiceHost.exe
+Net Start LR
+sc config LR start= auto
---
+++ WcfSample/WindowsServiceHost/Service1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using WcfService;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceHost
+{
+    public partial class LR : ServiceBase
+    {
+        public LR()
+        {
+            InitializeComponent();
+        }
+
+
+        private ServiceHost _host = new ServiceHost(typeof(MyService));
+
+        protected override void OnStart(string[] args)
+        {
+            _host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            _host.Close();
+        }
+    }
+}
